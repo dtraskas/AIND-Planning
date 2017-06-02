f09bb57ebeec0b6d3da5785a3957db7a76861c11
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-06-02 15:43:27 +0200

    Added h_ignore_preconditions() with comments

--- my_air_cargo_problems.py
+++ my_air_cargo_problems.py
@@ -223,8 +223,17 @@ class AirCargoProblem(Problem):
         conditions by ignoring the preconditions required for an action to be
         executed.
         """
-        # TODO implement (see Russell-Norvig Ed-3 10.2.3  or Russell-Norvig Ed-2 11.2)
+
+        # Get Knowledge Base and tell it about current state
+        kb = PropKB()
+        kb.tell(decode_state(node.state, self.state_map).pos_sentence())
+
+        # Since there are no preconditions then for every required fluent that is not present in current state
+        # there is one action, always available, that'll lead to it directly
         count = 0
+        for clause in self.goal:
+            if clause not in kb.clauses:
+                count += 1
         return count
 
 
