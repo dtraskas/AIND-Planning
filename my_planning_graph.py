6c17f0354f6b0a0d84b93e201adb258d3f2406b3
Mateusz Dziubek <mateusz.dziubek@schibsted.pl>
2017-06-04 11:19:05 +0200

    Implemented negation_mutex with comments

--- my_planning_graph.py
+++ my_planning_graph.py
@@ -510,8 +510,8 @@ class PlanningGraph():
         :param node_s2: PgNode_s
         :return: bool
         """
-        # TODO test for negation between nodes
-        return False
+        # S Nodes are mutually exclusive when their symbols are equal but their values are opposite
+        return node_s1.symbol == node_s2.symbol and node_s1.is_pos != node_s2.is_pos
 
     def inconsistent_support_mutex(self, node_s1: PgNode_s, node_s2: PgNode_s):
         """
